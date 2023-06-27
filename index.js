63fb45eebb3d7920c6eb42409cedc84349e3a32a
solo <soloman.err@.com>
2023-06-28 00:34:56 +0600

    implemented mongodb config & JWT token system

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 2000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 //middleware:
@@ -11,9 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 // JWT processor----------------------->>>>>>
-// const verifyJWT = (req, res, next) => {
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: 'Invalid authorization!' });
+  }
 
-// }
+  // token:
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: 'Invalid authorization!' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wndd9z6.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +48,7 @@ async function run() {
   try {
     const userCollection = client.db('brmmm').collection('users');
     const productCollection = client.db('brmmm').collection('products');
+    const cartCollection = client.db('brmmm').collection('cart');
 
     // JWT configuration----------------------->>>>>>
     app.post('/jwt', async (req, res) => {
@@ -58,9 +77,41 @@ async function run() {
       res.send(result);
     });
 
+    // Cart APIs----------------------------------->>>>>>
+    app.get('/carts', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+
+      const decodedEmail = req.decoded?.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: 'Unauthorized Access!' });
+      }
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
     // Get products---------------------------->>>>>>
     app.get('/products', async (req, res) => {
       const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
       res.send(result);
     });
 
